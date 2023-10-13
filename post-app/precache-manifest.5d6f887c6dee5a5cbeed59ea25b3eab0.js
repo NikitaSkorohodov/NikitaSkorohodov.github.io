4891e41e7d1e46c476476a28c255e48e94173f9e
NikitaSkorohodov <nikitaskoroho14@gmail.com>
2023-10-13 14:20:01 +0300

    react post-app

---
+++ post-app/precache-manifest.5d6f887c6dee5a5cbeed59ea25b3eab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79bacb71911a442ff3b5",
+    "url": "/post-app/static/css/main.669e2e2d.chunk.css"
+  },
+  {
+    "revision": "79bacb71911a442ff3b5",
+    "url": "/post-app/static/js/main.79bacb71.chunk.js"
+  },
+  {
+    "revision": "3e8606e4a39759b67e75",
+    "url": "/post-app/static/css/1.c9775f03.chunk.css"
+  },
+  {
+    "revision": "3e8606e4a39759b67e75",
+    "url": "/post-app/static/js/1.3e8606e4.chunk.js"
+  },
+  {
+    "revision": "6a8e189d228a5b7d6645",
+    "url": "/post-app/static/js/runtime~main.6a8e189d.js"
+  },
+  {
+    "revision": "c7fb531e1fd95c063a11306bcaab9759",
+    "url": "/post-app/index.html"
+  }
+];
