8d36c7dbeee56063fbff4df5dc174791de895b14
NikitaSkorohodov <nikitaskoroho14@gmail.com>
2023-10-13 13:17:19 +0300

    Updates

---
+++ precache-manifest.87f1623d2a1cad5cad270f5be34164a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79bacb71911a442ff3b5",
+    "url": "/static/css/main.669e2e2d.chunk.css"
+  },
+  {
+    "revision": "79bacb71911a442ff3b5",
+    "url": "/static/js/main.79bacb71.chunk.js"
+  },
+  {
+    "revision": "3e8606e4a39759b67e75",
+    "url": "/static/css/1.c9775f03.chunk.css"
+  },
+  {
+    "revision": "3e8606e4a39759b67e75",
+    "url": "/static/js/1.3e8606e4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9c6ffa3763e5ff3b65dffc0c02a8a16",
+    "url": "/index.html"
+  }
+];
